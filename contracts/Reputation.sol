fe0bd87e271b1e4c2bf5d8a239618dce3eb69a15
Dhanush <dhanushvallalar@gmail.com>
2025-02-24 02:40:48 +0530

    Contract update

--- contracts/Reputation.sol
+++ contracts/Reputation.sol
@@ -6,6 +6,7 @@ contract Reputation {
         uint256 completedJobs;
         uint256 totalRating;
         uint256 ratingCount;
+        string projectLink; // Add project link to the Freelancer struct
     }
 
     mapping(address => Freelancer) public freelancers;
@@ -23,5 +24,17 @@ contract Reputation {
         Freelancer storage freelancer = freelancers[_freelancer];
         if (freelancer.ratingCount == 0) return (0, freelancer.completedJobs);
         return (freelancer.totalRating / freelancer.ratingCount, freelancer.completedJobs);
+    }
+
+    // Add a function to allow freelancers to add a project link
+    function addProjectLink(string calldata _link) external {
+        Freelancer storage freelancer = freelancers[msg.sender];
+        freelancer.projectLink = _link;
+    }
+
+    // Add a function to retrieve the project link for a freelancer
+    function getProjectLink(address _freelancer) external view returns (string memory) {
+        Freelancer storage freelancer = freelancers[_freelancer];
+        return freelancer.projectLink;
     }
 }
